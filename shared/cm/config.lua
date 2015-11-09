76f5340a0c2558ab07f1cc3abf60de1790b02054
admin <admin>
2015-11-09 19:48:25 +0000

    Committing exported channel(s).

---
+++ shared/cm/config.lua
@@ -0,0 +1,71 @@
+-- This is a partly implemented next step in the channel manager to
+-- make an object which can load and save a configuration file so that
+-- we don't need to edit the source code of the channel manager to show
+-- the configuration directory.
+
+-- We're using some fancy parts of Lua called metatables which give us
+-- the hooks to do object orientated programming in Lua.
+
+if not cm then cm = {} end
+if not cm.config then cm.config = {} end
+
+local function ConfigName()
+   return iguana.workingDir()..'/IguanaRepo.cfg'
+end
+
+local function DefaultRepoLocation()
+   if os.isWindows() then
+      return "C:/iguana-web-apps/"
+   else
+      return "~/iguana-web-apps/"
+   end
+end
+
+local function ConfigDefault()
+   return {repo={DefaultRepoLocation()}}
+end
+
+local method = {}
+local meta={__index=method} 
+
+function method.load(S)
+   local Name = ConfigName()
+   if not os.fs.access(Name) then
+      S.config = ConfigDefault()
+      return
+   end
+   local Json = os.fs.readFile(Name)
+   if #Json == 0 then
+      S.config = ConfigDefault();
+      return
+   end
+   S.config = json.parse{data=Json}
+end
+
+function method.addRepo(S, Name, Path)
+   S.config.repo[#S.config.repo+1] = {name=Name, path=Path}
+end
+
+function method.save(S)
+   local Content = json.serialize{data=S.config}
+   os.fs.writeFile(ConfigName(), Content)
+end
+
+function method.clear(S)
+   S.config.repo = {}
+end
+
+function method.repoList(S)
+   local Result = {}
+   for i=1, #S.config.repo do
+      Result[i] = os.fs.name.toNative(S.config.repo[i])
+   end
+   return Result
+end
+
+function cm.config.open()
+   local T = {}
+   setmetatable(T, meta)
+   T:load()
+   return T  
+end
